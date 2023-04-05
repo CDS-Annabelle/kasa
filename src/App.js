9ab6a5cbbaf80eb5c6f151211b38072665fc5ca3
CDS <cdscontact@creation-de-site.eu>
2023-04-05 20:30:25 +0200

    edit footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import Content from './components/Content';
 import Footer from './components/Footer';
 import Header from './components/Header';
-
 import styles from "./App.module.scss";
 
 function App() {
   return (
     <div className={`d-flex flex-column ${styles.appContainer}`}>
       <Header />
-      {/* <Content />
-      <Footer /> */}
+      {/* <Content /> */}
+      <Footer />
     </div>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,14 @@
+import styles from "./Footer.module.scss";
+import logokasafooter from '../assets/images/logokasafooter.png';
+
 function Footer() {
-  return <div className="b3">Footer</div>;
+  return (
+  <footer 
+  className={`${styles.footer} d-flex flex-column align-items-center justify-content-center p-20`}>
+    <p><img src={logokasafooter} alt="logo Kasa" /></p>
+    <p>©2020 Kasa- All rights reserved</p>
+  </footer>
+  );
 }
 
 export default Footer;
