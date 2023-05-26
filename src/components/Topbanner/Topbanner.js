05cba1c294c6546cec98c021aec02526ac6d7099
CDS <cdscontact@creation-de-site.eu>
2023-05-26 12:08:36 +0200

    refactoring

--- src/components/Topbanner/Topbanner.js
+++ src/components/Topbanner/Topbanner.js
@@ -6,7 +6,7 @@ const Topbanner = ({ image, title }) => {
     <div className={styles.banner}>
       <img src={image} alt="" className={styles.banner__img} />
       <div className={styles.banner__overlay}>
-        <div className={styles.banner__text}>{title}</div>
+        <div className={`${styles.banner__text} ${styles.breakAfterComma}`}>{title}</div>
       </div>
     </div>
   );
